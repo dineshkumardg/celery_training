ea571539e6ecc35fe91c6e224a475f1936544abc
dineshkumardg <dineshdg87@gmail.com>
2022-02-08 13:44:16 +0530

    gevent changes

--- celery_beat_docker/celery_prj/tasks.py
+++ celery_beat_docker/celery_prj/tasks.py
@@ -8,6 +8,7 @@ app = Celery('tasks',
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
+    print('the sender %s $$$$$$' %sender)
     sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
 
     # Calls test('world') every 30 seconds
